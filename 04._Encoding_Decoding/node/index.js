4f1164ce421bddfd0e757736d57da19c3af0c919
Rasmus-Kibshede <2740ralle@gmail.com>
2024-02-08 10:35:51 +0100

    Modules and encode/decode

---
+++ 04._Encoding_Decoding/node/index.js
@@ -0,0 +1,7 @@
+const encode = btoa("Hello World")
+
+console.log(encode);
+
+const decode = atob(encode)
+
+console.log(decode);
---
+++ 04._Encoding_Decoding/python/main.py
@@ -0,0 +1,9 @@
+txt = "hallå"
+
+encoded_string = txt.encode()
+
+print(encoded_string)
+
+decoded_string = encoded_string.decode()
+
+print(decoded_string)
