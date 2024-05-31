14229107e675017af708c49121889157a0967fc1
Rasmus Kibshede <2740ralle@gmail.com>
2024-05-31 15:55:43 +0200

    new sse showcase

--- 00._Tasks/04._Real-time_communication_part_2/04a._SSE_Example/node/app.js
+++ 00._Tasks/04._Real-time_communication_part_2/04a._SSE_Example/node/app.js
@@ -1,49 +1,21 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
 
 const app = express();
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-app.use(bodyParser.json());
-
-let clients = {};
-let actClient = '';
-
-app.get('/chat/:name', (req, res) => {
+app.get("/synchhronize-time", (req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-    Connection: 'keep-alive',
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
   });
-  clients[req.params.name] = res;
-
-  req.on('close', () => {
-    delete clients[req.params.name];
-    sendMessagesToClients(`${req.params.name} has left the chat`, false);
-  });
-
-  sendMessagesToClients(`${req.params.name} has joined the chat`, false);
+  setInterval(() => sendTimeToClient(res), 1000);
 });
 
-app.post('/write', (req, res) => {
-  actClient = req.body.actClient;
-  sendMessagesToClients(req.body.message);
-  res.status(200).send('OK');
-});
-
-const sendMessagesToClients = (message, withActClient = true) => {
-  for (const client in clients) {
-    let data = '';
-    let date = new Date();
-    let timestamp = `${date.getHours()}:${date.getMinutes()}`;
-    if (withActClient) data = `${timestamp} ${actClient}: ${message}\n\n`;
-    else data = `${timestamp} ${message}\n\n`;
-
-    clients[client].write(`data: ${data} \n\n`);
-  }
+const sendTimeToClient = (res) => {
+  const time = new Date().toISOString();
+  res.write(`data: ${time} \n\n`);
 };
 
 const PORT = 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, console.log("Server is running on port:", PORT));
